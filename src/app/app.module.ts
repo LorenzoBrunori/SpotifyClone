f8bd01ec5fc473a27b43e948bfb72ddd6518c1ec
AndreaValeri <76973159+AndreaZenz@users.noreply.github.com>
2021-11-30 11:51:36 +0100

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeComponent } from './home/home.component';
+import { AlbumsComponent } from './Home/albums/albums.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AlbumsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
